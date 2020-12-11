f99e58c5556d2d0075d63643a63db57b6d37c6c8
Elvis Mercado <workscript@pm.me>
2020-12-11 13:23:22 +0100

    r

--- scripts/build-static-and-push-nginx-repo.sh
+++ scripts/build-static-and-push-nginx-repo.sh
@@ -1,9 +1,11 @@
 # Build static angular
 # Copy static files to testcovidmonitor repo
 # commit and push static files to testcovidmonitor repo
+
 npm run build:prod
 cp -Rv ./dist/bonaire-covid-monitor/browser/* ../testcovidmonitor
 
 cd ../testcovidmonitor
+git add -A
 git commit -m "static files update from script(build-static-and-push-nginx-repo)"
 git push
